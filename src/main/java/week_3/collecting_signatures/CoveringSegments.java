2fc5134eca08ad48b3f2da245c34a2cbbcd458a1
TomKetipov <tom.ketipov@gmail.com>
2023-11-24 16:19:17 +0200

    (Week_3) Add collecting_signatures solution/unit tests

--- src/main/java/week_3/collecting_signatures/CoveringSegments.java
+++ src/main/java/week_3/collecting_signatures/CoveringSegments.java
@@ -1,26 +1,51 @@
 package week_3.collecting_signatures;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class CoveringSegments {
-    private static int[] optimalPoints(Segment[] segments) {
-        //write your code here
-        int[] points = new int[2 * segments.length];
-        for (int i = 0; i < segments.length; i++) {
-            points[2 * i] = segments[i].start;
-            points[2 * i + 1] = segments[i].end;
-        }
-        return points;
-    }
+    /*
+            Collecting Signatures problem
+            You have to collect signatures from the tenants in the building.
+            You know the times each tenant will be in the building (represented by the segments in the problem)
+            and you want to minimize the number of visits and time spent at the building.
+            Assume that the actual visit with the tenant will take no time.
 
-    private static class Segment {
-        int start, end;
+            In other words, we have a bunch of line segments that may or may not overlap. We want to minimize the number of segments
+            =========================================================================================================================
+            0(n log n)
+     */
+
+    public static class Segment {
+        private int start, end;
 
         Segment(int start, int end) {
             this.start = start;
             this.end = end;
         }
     }
+
+    public static int[] optimalPoints(Segment[] segments) {
+        Arrays.sort(segments, Comparator.comparing(segment -> segment.end));
+
+        LinkedList<Integer> overlappingPointsList = new LinkedList<>();
+        overlappingPointsList.add(segments[0].end);
+
+        for (int i = 1; i < segments.length; i++) {
+            int start = segments[i].start;
+            int end = segments[i].end;
+
+            if (overlappingPointsList.getLast() < start || overlappingPointsList.getLast() > end) {
+                overlappingPointsList.add(end);
+            }
+        }
+        return overlappingPointsList.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
---
+++ src/test/java/week_3/collecting_signatures/CollectingSignaturesTest.java
@@ -0,0 +1,44 @@
+package week_3.collecting_signatures;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CollectingSignaturesTest {
+    private CoveringSegments coveringSegmentsSolver;
+
+    @Test
+    public void can_detect_single_overlapping_point() {
+        coveringSegmentsSolver = new CoveringSegments();
+
+        CoveringSegments.Segment[] segments = new CoveringSegments.Segment[3];
+        segments[0] = new CoveringSegments.Segment(1, 3);
+        segments[1] = new CoveringSegments.Segment(2, 5);
+        segments[2] = new CoveringSegments.Segment(3, 6);
+
+        Assert.assertArrayEquals(new int[]{3}, coveringSegmentsSolver.optimalPoints(segments));
+    }
+
+    @Test
+    public void can_detect_multiple_overlapping_points() {
+        coveringSegmentsSolver = new CoveringSegments();
+
+        CoveringSegments.Segment[] segments = new CoveringSegments.Segment[4];
+        segments[0] = new CoveringSegments.Segment(1, 3);
+        segments[1] = new CoveringSegments.Segment(4, 7);
+        segments[2] = new CoveringSegments.Segment(2, 5);
+        segments[3] = new CoveringSegments.Segment(5, 6);
+
+        Assert.assertArrayEquals(new int[]{3, 6}, coveringSegmentsSolver.optimalPoints(segments));
+    }
+
+    @Test
+    public void can_handle_single_segment() {
+        coveringSegmentsSolver = new CoveringSegments();
+
+        CoveringSegments.Segment[] segments = new CoveringSegments.Segment[1];
+        segments[0] = new CoveringSegments.Segment(0, 3);
+
+        Assert.assertTrue(coveringSegmentsSolver.optimalPoints(segments)[0] >= 0);
+        Assert.assertTrue(coveringSegmentsSolver.optimalPoints(segments)[0] <= 3);
+    }
+}
