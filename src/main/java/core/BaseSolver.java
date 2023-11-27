cbad9208cea62d966e3b2baffec30b8f5654c783
TomKetipov <tom.ketipov@gmail.com>
2023-11-27 07:55:34 +0200

    Add BaseSolver array validation methods

--- src/main/java/core/BaseSolver.java
+++ src/main/java/core/BaseSolver.java
@@ -1,5 +1,41 @@
 package core;
 
 public class BaseSolver {
-    // TODO
+
+    /**
+     * Validates an array input to ensure it is not null or empty.
+     *
+     * @param arr the array to be validated
+     * @throws IllegalArgumentException if the array is null or empty
+     */
+    protected void validate(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("Array input parameter cannot be null or empty.");
+        }
+    }
+
+    /**
+     * Validates an array input to ensure it is not null or empty.
+     *
+     * @param arr the array to be validated
+     * @throws IllegalArgumentException if the array is null or empty
+     */
+    protected void validate(String[] arr) {
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("Array input parameter cannot be null or empty.");
+        }
+    }
+
+
+    /**
+     * Validates an array input to ensure it is not null or empty.
+     *
+     * @param arr the array to be validated
+     * @throws IllegalArgumentException if the array is null or empty
+     */
+    protected void validate(char[] arr) {
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("Array input parameter cannot be null or empty.");
+        }
+    }
 }
