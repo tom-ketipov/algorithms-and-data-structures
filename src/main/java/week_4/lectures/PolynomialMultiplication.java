23770e5371265ad367abdc4f1726c8fb3e909886
TomKetipov <tom.ketipov@gmail.com>
2023-12-03 14:20:58 +0200

    (Week_4) Lectures -> polynomial_multiplication divide and conquer solution/unit tests

--- src/main/java/week_4/lectures/PolynomialMultiplication.java
+++ src/main/java/week_4/lectures/PolynomialMultiplication.java
@@ -1,9 +1,32 @@
 package week_4.lectures;
 
+import java.util.Arrays;
+
 public class PolynomialMultiplication {
 
-    // Time complexity: O(n^2)
+    /**
+     * Multiplies two polynomials using the naive or brute-force approach.
+     * The coefficients of the polynomials are represented as arrays, where the index
+     * of the array corresponds to the degree of the term, and the value at that index
+     * is the coefficient of the term. The resulting polynomial is represented by the
+     * multipliedCoefficients array.
+     * <p>
+     * The naive approach involves iterating through each term in the first polynomial
+     * and each term in the second polynomial, multiplying the corresponding coefficients,
+     * and accumulating the results in the appropriate positions of the product polynomial.
+     *
+     * @param coefficientsA The coefficients of the first polynomial.
+     * @param coefficientsB The coefficients of the second polynomial.
+     * @return The coefficients of the product polynomial.
+     * @throws IllegalArgumentException if either coefficientsA or coefficientsB is null.
+     *                                  Time complexity: O(n^2)
+     */
     public int[] multiplyPolynomialsNaive(int[] coefficientsA, int[] coefficientsB) {
+        // Validate input parameters
+        if (coefficientsA == null || coefficientsB == null) {
+            throw new IllegalArgumentException("Coefficients cannot be null.");
+        }
+
         int[] multipliedCoefficients = new int[coefficientsA.length + coefficientsB.length - 1];
 
         for (int i = 0; i < coefficientsA.length; i++) {
@@ -12,6 +35,98 @@ public class PolynomialMultiplication {
             }
         }
         return multipliedCoefficients;
+    }
+
+    /**
+     * Multiplies two polynomials using the Divide and Conquer algorithm.
+     * The coefficients of the polynomials are represented as arrays, where the index
+     * of the array corresponds to the degree of the term, and the value at that index
+     * is the coefficient of the term. The resulting polynomial is represented by the
+     * product array.
+     *
+     * @param coefficientsA The coefficients of the first polynomial.
+     * @param coefficientsB The coefficients of the second polynomial.
+     * @return The coefficients of the product polynomial.
+     * @throws IllegalArgumentException if either coefficientsA or coefficientsB is null.
+     *                                  <p>
+     *                                  Time complexity: O(n^2)
+     */
+    public int[] multiplyPolynomialsDivideAndConquer(int[] coefficientsA, int[] coefficientsB) {
+        // Validate input parameters
+        if (coefficientsA == null || coefficientsB == null) {
+            throw new IllegalArgumentException("Coefficients cannot be null.");
+        }
+
+        int degree = coefficientsA.length + coefficientsB.length - 1;
+        int[] product = new int[degree];
+
+        // Base case: If the polynomial has only one term
+        if (coefficientsA.length == 1) {
+            product[0] = coefficientsA[0] * coefficientsB[0];
+            return product;
+        }
+
+        // Equalize the coefficients length
+        int maxDegree = Math.max(coefficientsA.length, coefficientsB.length);
+        coefficientsA = Arrays.copyOf(coefficientsA, maxDegree);
+        coefficientsB = Arrays.copyOf(coefficientsB, maxDegree);
+
+        // Divide the coefficient arrays into 4 parts (A1, A0, B1, B0)
+        int halfLength = maxDegree / 2;
+        int[] a0 = Arrays.stream(coefficientsA, 0, halfLength).toArray();
+        int[] a1 = Arrays.stream(coefficientsA, halfLength, maxDegree).toArray();
+        int[] b0 = Arrays.stream(coefficientsB, 0, halfLength).toArray();
+        int[] b1 = Arrays.stream(coefficientsB, halfLength, maxDegree).toArray();
+
+        // Construct the new coefficient arrays by multiplying the halves
+        int[] a1b1 = multiplyPolynomialsNaive(a1, b1);
+        int[] a1b0 = multiplyPolynomialsNaive(a1, b0);
+        int[] a0b1 = multiplyPolynomialsNaive(a0, b1);
+        int[] a0b0 = multiplyPolynomialsNaive(a0, b0);
+
+        // Construct the middle array by adding (A1B0 + A0B1)
+        int[] middle = addArrays(a1b0, a0b1);
+
+        // Construct the product
+        for (int i = 0; i <= degree; i++) {
+            // Term from a0b0
+            if (i < a0b0.length) {
+                product[i] += a0b0[i];
+            }
+
+            // Term from middle array
+            int middleIndex = i + halfLength;
+            if (middleIndex < product.length && i < middle.length) {
+                product[middleIndex] += middle[i];
+            }
+
+            // Term from a1b1
+            int a1b1Index = (maxDegree % 2 == 1) ? (i + maxDegree - 1) : (i + maxDegree);
+            if (a1b1Index < product.length && i < a1b1.length && a1b1[i] != 0) {
+                product[a1b1Index] += a1b1[i];
+            }
+        }
+        return product;
+    }
+
+    /**
+     * Adds corresponding elements of two arrays element-wise and returns the result as a new array.
+     *
+     * @param array1 The first input array.
+     * @param array2 The second input array.
+     * @return A new array containing the element-wise sum of {@code array1} and {@code array2}.
+     * @throws IllegalArgumentException If the input arrays have different lengths.
+     */
+    private int[] addArrays(int[] array1, int[] array2) {
+        if (array1.length != array2.length) {
+            throw new IllegalArgumentException("Input arrays must have the same length.");
+        }
+
+        int[] result = new int[array1.length];
+        for (int i = 0; i < array1.length; i++) {
+            result[i] = array1[i] + array2[i];
+        }
+        return result;
     }
 }
 
--- src/test/java/week_4/lectures/PolynomialMultiplicationTest.java
+++ src/test/java/week_4/lectures/PolynomialMultiplicationTest.java
@@ -3,7 +3,10 @@ package week_4.lectures;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PolynomialMultiplicationTest {
     private PolynomialMultiplication polynomialMultiplicationSolver;
@@ -13,10 +16,12 @@ public class PolynomialMultiplicationTest {
         polynomialMultiplicationSolver = new PolynomialMultiplication();
     }
 
+    // Naive -> Linear approach
     @Test
     public void can_multiply_two_polynomials_with_coefficients_of_equal_length() {
         int[] coefficientsA = {1, 2, 3, 4};
         int[] coefficientsB = {4, 3, 2, 1};
+
         int[] result = polynomialMultiplicationSolver.multiplyPolynomialsNaive(coefficientsA, coefficientsB);
         assertArrayEquals(new int[]{4, 11, 20, 30, 20, 11, 4}, result);
     }
@@ -25,6 +30,7 @@ public class PolynomialMultiplicationTest {
     public void can_multiply_two_polynomials_with_coefficients_of_different_length() {
         int[] coefficientsA = {5, 0, 10, 6};
         int[] coefficientsB = {1, 2, 4};
+
         int[] result = polynomialMultiplicationSolver.multiplyPolynomialsNaive(coefficientsA, coefficientsB);
         assertArrayEquals(new int[]{5, 10, 30, 26, 52, 24}, result);
     }
@@ -33,7 +39,67 @@ public class PolynomialMultiplicationTest {
     public void can_multiply_two_polynomials_with_single_coefficient() {
         int[] coefficientsA = {5};
         int[] coefficientsB = {2};
+
         int[] result = polynomialMultiplicationSolver.multiplyPolynomialsNaive(coefficientsA, coefficientsB);
         assertArrayEquals(new int[]{10}, result);
+    }
+
+    @Test
+    public void naive_method_throws_exception_for_null_input() {
+        int[] coefficientsA = {3, 2, 2};
+
+        try {
+            polynomialMultiplicationSolver.multiplyPolynomialsDivideAndConquer(coefficientsA, null);
+        } catch (IllegalArgumentException e) {
+            assertEquals("Coefficients cannot be null.", e.getMessage());
+        }
+    }
+
+    // Divide and Conquer Naive
+    @Test
+    public void can_multiply_two_polynomials_with_single_coefficient_using_divide_and_conquer() {
+        int[] coefficientsA = {4};
+        int[] coefficientsB = {50};
+
+        int[] result = polynomialMultiplicationSolver.multiplyPolynomialsDivideAndConquer(coefficientsA, coefficientsB);
+        assertArrayEquals(new int[]{200}, result);
+    }
+
+    @Test
+    public void can_multiply_two_polynomials_with_coefficients_of_equal_even_length_using_divide_and_conquer() {
+        int[] coefficientsA = {1, 2, 3, 4};
+        int[] coefficientsB = {4, 3, 2, 1};
+
+        int[] result = polynomialMultiplicationSolver.multiplyPolynomialsDivideAndConquer(coefficientsA, coefficientsB);
+        assertArrayEquals(new int[]{4, 11, 20, 30, 20, 11, 4}, result);
+    }
+
+    @Test
+    public void can_multiply_two_polynomials_with_coefficients_of_different_length_using_divide_and_conquer() {
+        int[] coefficientsA = {5, 0, 10, 6};
+        int[] coefficientsB = {1, 2, 4};
+
+        int[] result = polynomialMultiplicationSolver.multiplyPolynomialsDivideAndConquer(coefficientsA, coefficientsB);
+        assertArrayEquals(new int[]{5, 10, 30, 26, 52, 24}, result);
+    }
+
+    @Test
+    public void can_multiply_two_polynomials_with_coefficients_of_equal_odd_lengths_using_divide_and_conquer() {
+        int[] coefficientsA = {1, 2, 3};
+        int[] coefficientsB = {3, 2, 2};
+
+        int[] result = polynomialMultiplicationSolver.multiplyPolynomialsDivideAndConquer(coefficientsA, coefficientsB);
+        assertArrayEquals(new int[]{3, 8, 15, 10, 6}, result);
+    }
+
+    @Test
+    public void divide_and_conquer_method_throws_exception_for_null_input() {
+        int[] coefficientsB = {3, 2, 2};
+
+        try {
+            polynomialMultiplicationSolver.multiplyPolynomialsDivideAndConquer(null, coefficientsB);
+        } catch (IllegalArgumentException e) {
+            assertEquals("Coefficients cannot be null.", e.getMessage());
+        }
     }
 }
