93a0711b8c9dd4fac6707b32ed95f6135c1ed30c
TomKetipov <tom.ketipov@gmail.com>
2023-12-04 17:24:51 +0200

    (Week_4) Lectures -> merge_sort solution/unit test

--- src/test/java/week_4/lectures/sort/ArraySortTest.java
+++ src/test/java/week_4/lectures/sort/ArraySortTest.java
@@ -5,17 +5,27 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-public class SortTest {
+public class ArraySortTest {
     private SelectionSort selectionSortSolver;
+    private MergeSort mergeSortSolver;
 
     @BeforeEach
     public void setUp() {
         selectionSortSolver = new SelectionSort();
+        mergeSortSolver = new MergeSort();
     }
 
     @Test
     public void can_sort_array_with_selection_sort() {
         int[] array = {2, 5, 1, 3, 23, 41};
+        assertArrayEquals(new int[]{1, 2, 3, 5, 23, 41}, selectionSortSolver.selectionSort(array));
+    }
+
+    @Test
+    public void can_sort_array_with_merge_sort() {
+        int[] array = {2, 5, 1, 3, 23, 41};
+
+        mergeSortSolver.mergeSort(array);
         assertArrayEquals(new int[]{1, 2, 3, 5, 23, 41}, selectionSortSolver.selectionSort(array));
     }
 }
