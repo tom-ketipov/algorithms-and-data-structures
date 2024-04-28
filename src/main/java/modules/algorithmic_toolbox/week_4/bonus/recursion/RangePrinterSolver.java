f9f3fe3c10a0bf68ae39640c3292fd91d6357a01
TomKetipov <tom.ketipov@gmail.com>
2024-04-28 15:30:08 +0300

    (Module #1/Week #4/Bonus) Add solver that prints numbers in range without use of loops

---
+++ src/main/java/modules/algorithmic_toolbox/week_4/bonus/recursion/RangePrinterSolver.java
@@ -0,0 +1,27 @@
+package modules.algorithmic_toolbox.week_4.bonus.recursion;
+
+/*
+        Print 1 to n without using loops
+        You are given an integer N. Print numbers from 1 to N without the help of loops.
+ */
+public class RangePrinterSolver {
+
+    /**
+     * Prints integers within a specified range.
+     *
+     * @param from the starting integer of the range
+     * @param to   the ending integer of the range
+     */
+    public static void printNumbersInRange(int from, int to) {
+        if (from > to) {
+            return;
+        }
+
+        System.out.println(from);
+        printNumbersInRange(from + 1, to);
+    }
+
+    public static void main(String[] args) {
+        printNumbersInRange(1, 5);
+    }
+}
