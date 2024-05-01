fc5e9658d2d7787d419af04b4f12d69806468698
TomKetipov <tom.ketipov@gmail.com>
2024-05-01 16:16:54 +0300

    (Module #1/Week #4/ Bonus) Recursive array element printer

---
+++ src/main/java/modules/algorithmic_toolbox/week_4/bonus/recursion/ArrayPrinter.java
@@ -0,0 +1,20 @@
+package modules.algorithmic_toolbox.week_4.bonus.recursion;
+
+public class ArrayPrinter {
+
+    public static void printArray(int[] array) {
+        printArray(array, 0);
+    }
+
+    private static void printArray(int[] array, int currentIndex) {
+        if (currentIndex < array.length) {
+            System.out.println(array[currentIndex]);
+            printArray(array, currentIndex + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 5, 8, 19};
+        printArray(array);
+    }
+}
