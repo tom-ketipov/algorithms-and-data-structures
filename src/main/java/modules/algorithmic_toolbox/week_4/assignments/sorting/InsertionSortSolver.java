8a4b98ca9a94c8d7261015ea7487bc766f7968e0
TomKetipov <tom.ketipov@gmail.com>
2024-05-15 07:05:04 +0300

    (Module #1/Week #4) Insertion sort implementation + unit test coverage

--- src/main/java/modules/algorithmic_toolbox/week_4/assignments/sorting/InsertionSortSolver.java
+++ src/main/java/modules/algorithmic_toolbox/week_4/assignments/sorting/InsertionSortSolver.java
@@ -1,4 +1,27 @@
 package modules.algorithmic_toolbox.week_4.assignments.sorting;
 
+import enums.ValidationMessageType;
+import org.apache.commons.lang3.Validate;
+
 public class InsertionSortSolver {
+
+    /**
+     * Sorts an integer array in non-decreasing order using the Insertion Sort algorithm.
+     *
+     * @param arr the array to be sorted
+     */
+    public void sort(int[] arr) {
+        Validate.notNull(arr, ValidationMessageType.NULL_VALUE_ERROR.getMessage());
+
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
 }
--- src/test/java/modules/algorithmic_toolbox/week_4/assignments/sorting/SortSolverTest.java
+++ src/test/java/modules/algorithmic_toolbox/week_4/assignments/sorting/SortSolverTest.java
@@ -8,10 +8,12 @@ import org.junit.jupiter.api.Test;
 class SortSolverTest {
 
     private SelectionSortSolver selectionSortSolver;
+    private InsertionSortSolver insertionSortSolver;
 
     @BeforeEach
     void setUp() {
         selectionSortSolver = new SelectionSortSolver();
+        insertionSortSolver = new InsertionSortSolver();
     }
 
     @Test
@@ -34,7 +36,32 @@ class SortSolverTest {
     public void should_throw_null_pointer_exception_for_null_input_in_selection_sort_method() {
         try {
             selectionSortSolver.sort(null);
-        }catch (NullPointerException e){
+        } catch (NullPointerException e) {
+            Assertions.assertEquals(ValidationMessageType.NULL_VALUE_ERROR.getMessage(), e.getMessage());
+        }
+    }
+
+    @Test
+    public void should_be_able_to_sort_an_array_with_integers_with_insertion_sort() {
+        int[] array = {8, 4, 2, 5, 2};
+
+        insertionSortSolver.sort(array);
+        Assertions.assertArrayEquals(new int[]{2, 2, 4, 5, 8}, array);
+    }
+
+    @Test
+    public void should_return_input_array_for_sorted_array_inputs_with_insertion_sort() {
+        int[] array = {2, 2, 3, 4, 5};
+
+        insertionSortSolver.sort(array);
+        Assertions.assertArrayEquals(new int[]{2, 2, 3, 4, 5}, array);
+    }
+
+    @Test
+    public void should_throw_null_pointer_exception_for_null_input_in_insertion_sort_method() {
+        try {
+            insertionSortSolver.sort(null);
+        } catch (NullPointerException e) {
             Assertions.assertEquals(ValidationMessageType.NULL_VALUE_ERROR.getMessage(), e.getMessage());
         }
     }
