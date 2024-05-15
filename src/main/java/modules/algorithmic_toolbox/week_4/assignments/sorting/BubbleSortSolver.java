3af4b861bf130d65bf3afe5025c2b93a83de4d30
TomKetipov <tom.ketipov@gmail.com>
2024-05-15 07:23:28 +0300

    (Module #1/Week #4) Bubble sort implementation + unit test coverage

--- src/main/java/modules/algorithmic_toolbox/week_4/assignments/sorting/BubbleSortSolver.java
+++ src/main/java/modules/algorithmic_toolbox/week_4/assignments/sorting/BubbleSortSolver.java
@@ -1,4 +1,28 @@
 package modules.algorithmic_toolbox.week_4.assignments.sorting;
 
-public class BubbleSortSolver {
+import enums.ValidationMessageType;
+import modules.core.BaseSolver;
+import org.apache.commons.lang3.Validate;
+
+public class BubbleSortSolver extends BaseSolver {
+
+    /**
+     * Sorts an integer array in non-decreasing order using the Bubble Sort algorithm.
+     *
+     * @param arr the array to be sorted
+     * @throws IllegalArgumentException if the input array is null
+     */
+    public void sort(int[] arr) {
+        Validate.notNull(arr, ValidationMessageType.NULL_VALUE_ERROR.getMessage());
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int currentPosition = 0;
+            while (currentPosition < i) {
+                if (arr[currentPosition] > arr[currentPosition + 1]) {
+                    swap(arr, currentPosition, currentPosition + 1);
+                }
+                currentPosition++;
+            }
+        }
+    }
 }
--- src/test/java/modules/algorithmic_toolbox/week_4/assignments/sorting/SortSolverTest.java
+++ src/test/java/modules/algorithmic_toolbox/week_4/assignments/sorting/SortSolverTest.java
@@ -5,17 +5,22 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 class SortSolverTest {
 
     private SelectionSortSolver selectionSortSolver;
     private InsertionSortSolver insertionSortSolver;
+    private BubbleSortSolver bubbleSortSolver;
 
     @BeforeEach
     void setUp() {
         selectionSortSolver = new SelectionSortSolver();
         insertionSortSolver = new InsertionSortSolver();
+        bubbleSortSolver = new BubbleSortSolver();
     }
 
+    // Selection sort
     @Test
     public void should_be_able_to_sort_an_array_with_integers_with_selection_sort() {
         int[] array = {8, 4, 2, 5, 2};
@@ -41,6 +46,7 @@ class SortSolverTest {
         }
     }
 
+    // Insertion sort
     @Test
     public void should_be_able_to_sort_an_array_with_integers_with_insertion_sort() {
         int[] array = {8, 4, 2, 5, 2};
@@ -61,6 +67,33 @@ class SortSolverTest {
     public void should_throw_null_pointer_exception_for_null_input_in_insertion_sort_method() {
         try {
             insertionSortSolver.sort(null);
+        } catch (NullPointerException e) {
+            Assertions.assertEquals(ValidationMessageType.NULL_VALUE_ERROR.getMessage(), e.getMessage());
+        }
+    }
+
+    // Bubble sort
+    @Test
+    public void should_be_able_to_sort_an_array_with_integers_with_bubble_sort() {
+        int[] array = {8, 4, 2, 5, 2};
+
+        bubbleSortSolver.sort(array);
+        System.out.println(Arrays.toString(array));
+        Assertions.assertArrayEquals(new int[]{2, 2, 4, 5, 8}, array);
+    }
+
+    @Test
+    public void should_return_input_array_for_sorted_array_inputs_with_bubble_sort() {
+        int[] array = {2, 2, 3, 4, 5};
+
+        bubbleSortSolver.sort(array);
+        Assertions.assertArrayEquals(new int[]{2, 2, 3, 4, 5}, array);
+    }
+
+    @Test
+    public void should_throw_null_pointer_exception_for_null_input_in_bubble_sort_method() {
+        try {
+            bubbleSortSolver.sort(null);
         } catch (NullPointerException e) {
             Assertions.assertEquals(ValidationMessageType.NULL_VALUE_ERROR.getMessage(), e.getMessage());
         }
